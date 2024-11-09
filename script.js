d7af99750880a9e947e77f141443e693a6e37337
JulioCesaarr <julio10cbsantos@gmail.com>
2024-11-09 01:53:08 -0300

    Adicionando responvidade na pag de login e cadastr

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentIndex = 0;
+const images = document.querySelectorAll(".carousel img");
+
+function showNextImage() {
+images.forEach((img, index) => {
+img.classList.remove("active");
+if (index === currentIndex) {
+img.classList.add("active");
+}
+});
+currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(showNextImage, 3000); 
